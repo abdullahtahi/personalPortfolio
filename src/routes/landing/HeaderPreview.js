66fa151ec99d574f8ba7b0a1ce4c691da8f1a97a
Abdullah Tahir <tahirabdullah801@gmail.comm>
2023-01-22 14:01:55 +0500

    wokring with the name

--- src/routes/landing/HeaderPreview.js
+++ src/routes/landing/HeaderPreview.js
@@ -5,8 +5,8 @@ const HeaderPreview = () => {
     <article className="header">
       <h1 className="header-font white-text h1-tag">
         Hi, I'm <br></br>
-        <em className="pink-text">Abdullah Tahir</em>
-        <br></br>Web Developer
+        <em className="pink-text">Sidra Tahir</em>
+        <br></br>MERN Developer
       </h1>
       <p className="gray-text p-tag">Front End Development</p>
       <Link className="pink-text" to="/portfolio">
